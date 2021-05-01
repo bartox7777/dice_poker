232df924cc8ee60d57e4e2895e3f47161fc3af48
Bartosz Z <bartosz.zelek@protonmail.com>
2021-05-01 22:13:03 +0200

    Corrections of typos

--- game.py
+++ game.py
@@ -2,7 +2,7 @@
 import sys
 import pygame
 from random import randint
-from objects import Dice
+from objects import Die
 
 
 pygame.init()
@@ -13,15 +13,14 @@ Y_SPACE = 120
 def get_random_dice(number=5):
     random_dice = []
     for i in range(number):
-        random_dice.append(Dice(randint(1, 6), X, (i+1)*Y_SPACE))
+        random_dice.append(Die(randint(1, 6), X, (i+1)*Y_SPACE))
     return random_dice
 
 screen = pygame.display.set_mode(size=(1900, 1000), flags=pygame.RESIZABLE)
 pygame.display.set_caption("Dice poker")
-pygame.display.set_icon(pygame.image.load("dice_6.png"))
+pygame.display.set_icon(pygame.image.load("die_6.png"))
 shuffle = True
 random_dice = get_random_dice()
-print(random_dice)
 
 while True:
     for event in pygame.event.get():
@@ -31,6 +30,6 @@ while True:
             random_dice = get_random_dice()
 
     screen.fill((48, 128, 72))
-    for dice in random_dice:
-        dice.draw(screen)
+    for die in random_dice:
+        die.draw(screen)
     pygame.display.flip()
--- objects.py
+++ objects.py
@@ -4,21 +4,21 @@ import pygame
 pygame.init()
 
 
-class Dice:
-    def __init__(self, dice_number=1, x=0, y=0):
-        self.dice_number = dice_number
-        if dice_number not in range(1, 7):
-            raise Exception("dice_number must be between 1 and 6")
-        dice_images = ["dice_{}.png".format(i+1) for i in range(6)]
-        self.dice = pygame.image.load(dice_images[dice_number-1])
-        self.dice_rect = self.dice.get_rect(x=x, y=y)
+class Die:
+    def __init__(self, die_number=1, x=0, y=0):
+        self.die_number = die_number
+        if die_number not in range(1, 7):
+            raise Exception("die_number must be between 1 and 6")
+        dice_images = ["die_{}.png".format(i+1) for i in range(6)]
+        self.die = pygame.image.load(dice_images[die_number-1])
+        self.die_rect = self.die.get_rect(x=x, y=y)
 
     def draw(self, screen, x=None, y=None):
         if x is not None:
-            self.dice_rect.x = x
+            self.die_rect.x = x
         if y is not None:
-            self.dice_rect.y = y
-        screen.blit(self.dice, self.dice_rect)
+            self.die_rect.y = y
+        screen.blit(self.die, self.die_rect)
 
     def __repr__(self):
-        return "Dice {}".format(self.dice_number)
+        return "Die {}".format(self.die_number)
