ae0db607d267f34eab6f949814889a9f6a654f18
Bartosz Z <bartosz.zelek@protonmail.com>
2021-05-01 21:08:26 +0200

    Shuffle and draw dice

--- game.py
+++ game.py
@@ -1,21 +1,36 @@
 # POKEROWE KOŚCI (inspiracja: https://www.kurnik.pl/kosci/)
 import sys
 import pygame
+from random import randint
 from objects import Dice
 
 
 pygame.init()
+X = 1200
+Y_SPACE = 120
+
+
+def get_random_dice(number=5):
+    random_dice = []
+    for i in range(number):
+        random_dice.append(Dice(randint(1, 6), X, (i+1)*Y_SPACE))
+    return random_dice
 
 screen = pygame.display.set_mode(size=(1900, 1000), flags=pygame.RESIZABLE)
-# dice = pygame.image.load("dice_1.png")
-# dice_rect = ball.get_rect()
-test_dice = Dice("dice_1.png")
+pygame.display.set_caption("Dice poker")
+pygame.display.set_icon(pygame.image.load("dice_6.png"))
+shuffle = True
+random_dice = get_random_dice()
+print(random_dice)
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            random_dice = get_random_dice()
 
     screen.fill((48, 128, 72))
-    test_dice.draw(screen)
+    for dice in random_dice:
+        dice.draw(screen)
     pygame.display.flip()
--- objects.py
+++ objects.py
@@ -5,9 +5,20 @@ pygame.init()
 
 
 class Dice:
-    def __init__(self, image_path, x=0, y=0):
-        self.dice = pygame.image.load(image_path)
+    def __init__(self, dice_number=1, x=0, y=0):
+        self.dice_number = dice_number
+        if dice_number not in range(1, 7):
+            raise Exception("dice_number must be between 1 and 6")
+        dice_images = ["dice_{}.png".format(i+1) for i in range(6)]
+        self.dice = pygame.image.load(dice_images[dice_number-1])
         self.dice_rect = self.dice.get_rect(x=x, y=y)
 
     def draw(self, screen, x=None, y=None):
+        if x is not None:
+            self.dice_rect.x = x
+        if y is not None:
+            self.dice_rect.y = y
         screen.blit(self.dice, self.dice_rect)
+
+    def __repr__(self):
+        return "Dice {}".format(self.dice_number)
