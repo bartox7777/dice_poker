9feb2b0e78e525a819191cf22b34b0ca2b2864c7
Bartosz Z <bartosz.zelek@protonmail.com>
2021-05-02 23:21:15 +0200

    Preparings for us Sprites and Groups

--- game.py
+++ game.py
@@ -7,19 +7,21 @@ from objects import Die
 
 
 pygame.init()
-X = 1200
-Y_SPACE = 120
+DICE_X = 1200
+DICE_Y_SPACE = 120
+GREEN_BG = (48, 128, 72)
+DIE_6_PATH = os.path.join("data", "die_6.png")
 
 
 def get_random_dice(number=5):
     random_dice = []
     for i in range(number):
-        random_dice.append(Die(randint(1, 6), X, (i+1)*Y_SPACE))
+        random_dice.append(Die(randint(1, 6), DICE_X, (i+1)*DICE_Y_SPACE))
     return random_dice
 
 screen = pygame.display.set_mode(size=(1900, 1000), flags=pygame.RESIZABLE)
 pygame.display.set_caption("Dice poker")
-pygame.display.set_icon(pygame.image.load(os.path.join("data", "die_6.png")))
+pygame.display.set_icon(pygame.image.load(DIE_6_PATH))
 shuffle = True
 random_dice = get_random_dice()
 
@@ -30,7 +32,7 @@ while True:
         if event.type == pygame.MOUSEBUTTONDOWN:
             random_dice = get_random_dice()
 
-    screen.fill((48, 128, 72))
+    screen.fill(GREEN_BG)
     for die in random_dice:
         die.draw(screen)
     pygame.display.flip()
