1acadde10aebfb504e4124ceefb251806d0c5b3d
Bartosz Z <bartosz.zelek@protonmail.com>
2021-05-03 13:51:21 +0200

    Draw a background table

--- game.py
+++ game.py
@@ -3,25 +3,33 @@ import os
 import sys
 import pygame
 from random import randint
-from objects import Die, Dice
+from objects import Die, Dice, Table
 
 
 pygame.init()
 DICE_X = 1200
 DICE_Y_SPACE = 120
+DICE_PADDING_TOP = 70
 GREEN_BG = (48, 128, 72)
+DARK_WHITE = (220, 220, 210)
 DIE_6_PATH = os.path.join("data", "die_6.png")
 
 
 def get_random_dice(DiceGroup, number_of_dice=5):
     DiceGroup.empty()
     for i in range(number_of_dice):
-        DiceGroup.add(Die(randint(1, 6), DICE_X, (i+1)*DICE_Y_SPACE))
+        DiceGroup.add(Die(randint(1, 6), DICE_X, (i+1)*DICE_Y_SPACE+DICE_PADDING_TOP))
 
-screen = pygame.display.set_mode(size=(1900, 1000), flags=pygame.RESIZABLE)
 pygame.display.set_caption("Dice poker")
 pygame.display.set_icon(pygame.image.load(DIE_6_PATH))
+
+screen = pygame.display.set_mode(size=(1900, 1000), flags=pygame.RESIZABLE)
+
 dice_group = Dice()
+table = Table(screen, DARK_WHITE, 500, 120, 400, 700)
+# table_group = TableGroup()
+# table_group.add(table)
+
 shuffle = True
 get_random_dice(dice_group)
 
@@ -34,4 +42,5 @@ while True:
 
     screen.fill(GREEN_BG)
     dice_group.draw(screen)
+    table.draw()
     pygame.display.flip()
--- objects.py
+++ objects.py
@@ -1,22 +1,24 @@
 import os
 import pygame
-from pygame.sprite import Sprite, Group
+from pygame.sprite import Sprite, Group, GroupSingle
 
 
 pygame.init()
 
-
 class Die(Sprite):
-    def __init__(self, die_number=1, x=0, y=0):
-        super().__init__()  # Sprite.__init__(self)
+    def __init__(self, die_number, x=0, y=0):
+        super().__init__()
         self.die_number = die_number
+
         if die_number not in range(1, 7):
             raise Exception("die_number must be between 1 and 6")
         dice_images = [os.path.join("data", "die_{}.png".format(i+1)) for i in range(6)]
+
         try:
             self.image = pygame.image.load(dice_images[die_number-1]).convert_alpha()
         except:
             raise Exception("Couldn't load image {}".format(dice_images[die_number-1]))
+
         self.rect = self.image.get_rect(x=x, y=y)
 
     def __repr__(self):
@@ -26,3 +28,22 @@ class Die(Sprite):
 class Dice(Group):
     def __init__(self):
         super().__init__()
+
+
+class Table(Sprite):
+    def __init__(self, surface, color, topleft_x, topleft_y, width, height):
+        super().__init__()
+        self.surface = surface
+        self.color = color
+        self.topleft_x = topleft_x
+        self.topleft_y = topleft_y
+        self.width = width
+        self.height = height
+    
+    def draw(self):
+        pygame.draw.rect(self.surface, self.color, pygame.Rect(self.topleft_x, self.topleft_y, self.width, self.height))
+
+
+# class TableGroup(GroupSingle):
+#     def __init__(self):
+#         super().__init__()
